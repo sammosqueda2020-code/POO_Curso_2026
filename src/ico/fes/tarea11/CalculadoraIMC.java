6cd709579be0427da770591ec0a169c86ed36ec3
sammosqueda2020-code <sammosqueda2020@gmail.com>
2026-04-28 08:41:16 -0600

    Update CalculadoraIMC.java

--- src/ico/fes/tarea11/CalculadoraIMC.java
+++ src/ico/fes/tarea11/CalculadoraIMC.java
@@ -9,7 +9,7 @@ public class CalculadoraIMC extends Frame {
     private Button btnCalcular;
 
     public CalculadoraIMC() {
-        super("Calculadora de IMC");
+        super("Calculadora de IMC - Samuel Mosqueda");
         this.setSize(300, 250);
         this.setLayout(new GridLayout(4, 2, 10, 10));
 
@@ -27,25 +27,20 @@ public class CalculadoraIMC extends Frame {
                     float peso = Float.parseFloat(txtPeso.getText());
                     float altura = Float.parseFloat(txtAltura.getText());
                     float imc = peso / (altura * altura);
-                    lblResultado.setText(String.format("Resultado: %.2f", imc));
+                    lblResultado.setText(String.format("IMC: %.2f", imc));
                 } catch (NumberFormatException ex) {
-                    lblResultado.setText("Error: Datos inválidos");
+                    lblResultado.setText("Error: Datos invalidos");
                 }
             }
         });
 
-        this.add(lblPeso);
-        this.add(txtPeso);
-        this.add(lblAltura);
-        this.add(txtAltura);
-        this.add(new Label("")); 
-        this.add(btnCalcular);
+        this.add(lblPeso); this.add(txtPeso);
+        this.add(lblAltura); this.add(txtAltura);
+        this.add(new Label("")); this.add(btnCalcular);
         this.add(lblResultado);
 
         this.addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent e) {
-                System.exit(0);
-            }
+            public void windowClosing(WindowEvent e) { System.exit(0); }
         });
 
         this.setVisible(true);
